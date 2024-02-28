c80161763c0a3feff725907e62a987025bdd8881
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-28 16:05:42 +0100

    Added methods for many to many on models

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function project()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
