d4423b4f05dfa18f62d07ca12183e9ac021a55a8
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-28 15:51:58 +0100

    Populated table with Seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,22 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array = [
+            'HTML',
+            'CSS',
+            'Javascript',
+            'VueJs',
+            'PHP',
+            'Laravel'
+        ];
+
+        foreach ($array as $item) {
+            $technology = new Technology();
+
+            $technology->name = $item;
+            $technology->slug = Str::slug($item, '-');
+
+            $technology->save();
+        }
     }
 }
